1bb70a694d8cd48b2e20f9fddaa836d93e7f9f54
Joseph Huckaby <jhuckaby@gmail.com>
2023-03-16 21:23:55 -0700

    Delete mail temp files after 7 days

--- bin/backup.sh
+++ bin/backup.sh
@@ -13,3 +13,5 @@ export HOME
 # /usr/bin/aws s3 sync /data/logs/archives/ s3://mendo.org/logs/ --quiet
 find /data/logs/archives/ -type f -name '*.gz' -mtime +30 -delete >/dev/null 2>&1
 
+# Delete mail temp files after 7 days
+find /data/mail/ -type f -name '*.json' -mtime +1 -delete >/dev/null 2>&1
